d86c377028142e158a0982734ed1604b92b5f766
Chizuru <chizuru.yamauchi@dci-student.org>
2024-06-26 16:27:21 +0200

    add source code in ProductService

--- src/main/java/com/example/onlineshop/service/ProductService.java
+++ src/main/java/com/example/onlineshop/service/ProductService.java
@@ -1,4 +1,56 @@
 package com.example.onlineshop.service;
 
+import com.example.onlineshop.models.Product;
+import com.example.onlineshop.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CurrencyConverterService currencyConverterService;
+
+    public Product addProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product productDetails){
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found for id: " + id));
+        product.setName(productDetails.getName());
+        product.setDescription(productDetails.getDescription());
+        product.setPrice(productDetails.getPrice());
+        product.setCurrency(productDetails.getCurrency());
+
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id){
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found for id: " + id));
+        productRepository.delete(product);
+    }
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id){
+        return productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found for id: " + id));
+    }
+
+    public BigDecimal convertProductPrice(Long productId, String targetCurrency){
+        Product product = getProductById(productId);
+        BigDecimal price = BigDecimal.valueOf(product.getPrice());
+        return currencyConverterService.convertCurrency(product.getCurrency(), targetCurrency, price);
+    }
+
 }
