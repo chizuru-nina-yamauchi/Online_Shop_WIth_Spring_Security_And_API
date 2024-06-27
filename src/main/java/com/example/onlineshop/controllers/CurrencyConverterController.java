561039540e2db01e02382332a25b852cae99891e
Chizuru <chizuru.yamauchi@dci-student.org>
2024-06-27 14:00:25 +0200

    add source code in CurrencyConverterController file

--- src/main/java/com/example/onlineshop/controllers/CurrencyConverterController.java
+++ src/main/java/com/example/onlineshop/controllers/CurrencyConverterController.java
@@ -1,4 +1,25 @@
 package com.example.onlineshop.controllers;
 
+import com.example.onlineshop.service.CurrencyConverterService;
+import com.example.onlineshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+
+@Controller
 public class CurrencyConverterController {
+    @Autowired// Dependency Injection
+    private ProductService productService;
+
+    @GetMapping("/convert-price")
+    public String showCurrencyConverter(@RequestParam Long productId, @RequestParam String targetCurrency, Model model) {
+        BigDecimal convertedPrice = productService.convertProductPrice(productId, targetCurrency);
+        model.addAttribute("convertedPrice", convertedPrice); // Passing the converted Price to the view. This makes available the convertedPrice to the Thymeleaf view(HTML file).
+        return "currency-converter";
+    }
 }
+// We don't have to create @Postmapping because we are not changing anything in the database. We are just converting the currency.
